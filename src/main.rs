e9f89cb8c479c3e95d616e47f22565347ca92f52
Ian Aranza <ian.aranza@gmail.com>
2021-09-09 14:24:55 -0400

    Reverse the todo file and print untill the --- delimiter.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::{
+  fs::File,
+  io::{BufRead, BufReader},
+};
+
+use rev_lines::RevLines;
+
 fn main() {
-    println!("Hello, world!");
+  let todo_file = File::open("/home/ipa/todo").expect("Failed to read todo file.");
+  let rev_lines = RevLines::new(BufReader::new(todo_file)).unwrap();
+  for line in rev_lines {
+    if line.starts_with("---") {
+      return;
+    }
+    println!("{}", line);
+  }
 }
