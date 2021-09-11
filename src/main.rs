c8e70b8ad2e6ca2295cdd502b48c5aacaa41614e
Ian Aranza <ian.aranza@gmail.com>
2021-09-11 19:51:10 -0400

    Add support for osx home directory.

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,12 @@ use std::{fs::File, io::BufReader};
 
 use rev_lines::RevLines;
 
+
 fn main() {
-  let todo_file = File::open("/home/ipa/todo").expect("Failed to read todo file.");
+  #![allow(deprecated)] // Deprecated because of  windows support.
+  let mut todo_file = std::env::home_dir().expect("Failed to get home dir.");
+  todo_file.push("todo");
+  let todo_file = File::open(todo_file).expect("Failed to read todo file.");
   let rev_lines = RevLines::new(BufReader::new(todo_file)).unwrap();
   for line in rev_lines {
     if line.starts_with("---") {
